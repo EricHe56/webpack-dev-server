89853a7bd44dc4a51ae6529093c694e0819c20be
EH <eric.he@tinycloud.uk>
2026-05-19 20:52:39 +0100

    fix: CVE-2025-30360 & verify

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    // 明确指定允许访问的主机
+    // 这将确保我们的合法验证请求能够通过主机检查
+    allowedHosts: ['localhost', '127.0.0.1'],
+  },
 };
